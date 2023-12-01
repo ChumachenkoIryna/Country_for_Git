3aeee90c39ef7306feb5a3cdef8329916bc117d5
IRISKA <chumachenko.bc@gmail.com>
2023-12-01 18:57:31 +0200

    # Task_1 . Создание класса  Country
    Создание класса Country со свойствами Id, Name,  Capital , Population,  Area

---
+++ Country_for_Git/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Country_for_Git
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public int? Population { get; set; }
+        public double? Area { get; set; }
+
+        
+    }
+}
