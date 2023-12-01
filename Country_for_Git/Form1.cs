b2736e576734a06506d04cfd6ebb2c204f9aad33
IRISKA <chumachenko.bc@gmail.com>
2023-12-01 19:31:41 +0200

    #Task_ 5 Добавление конструктора формы
    Добавление списка стран и частей света в форму

--- Country_for_Git/Form1.cs
+++ Country_for_Git/Form1.cs
@@ -5,6 +5,37 @@ namespace Country_for_Git
         public Form1()
         {
             InitializeComponent();
+            try
+            {
+                using (var db = new WorldPartContext())
+                {
+                    var query = from b in db.WorldParts
+                                select b;
+                    comboBoxWorldPart.DataSource = query.ToList();
+                    comboBoxWorldPart.DisplayMember = "Name";
+
+                    var query2 = from b in db.Countries
+                                 select b;
+                    comboBoxCountry.DataSource = query2.ToList();
+                    comboBoxCountry.DisplayMember = "Name";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            //using(var db = new WorldPartContext())
+            //{
+            //WorldPart part1 = new WorldPart { Name = "Европа" };
+            //WorldPart part2 = new WorldPart { Name = "Азия" };
+            //WorldPart part3 = new WorldPart { Name = "Африка" };
+            //WorldPart part4 = new WorldPart { Name = "Европа" };
+            //db.WorldParts.Add(part1);
+            //db.WorldParts.Add(part2);
+            //db.WorldParts.Add(part3);
+            //db.WorldParts.Add(part4);
+            //}
         }
     }
 }
