9fdbcbf331d30cae7c72f20b63750971f9662384
User_2 <chumachenko.bc@gmail.com>
2023-12-14 18:47:19 +0200

    #Task1_2 Удаление ненужных комментариев

--- Country_for_Git/Form1.cs
+++ Country_for_Git/Form1.cs
@@ -25,17 +25,6 @@ namespace Country_for_Git
                 MessageBox.Show(ex.Message);
             }
 
-            //using(var db = new WorldPartContext())
-            //{
-            //WorldPart part1 = new WorldPart { Name = "Европа" };
-            //WorldPart part2 = new WorldPart { Name = "Азия" };
-            //WorldPart part3 = new WorldPart { Name = "Африка" };
-            //WorldPart part4 = new WorldPart { Name = "Европа" };
-            //db.WorldParts.Add(part1);
-            //db.WorldParts.Add(part2);
-            //db.WorldParts.Add(part3);
-            //db.WorldParts.Add(part4);
-            //}
         }
 
         private void AddGroupClick(object sender, EventArgs e)
